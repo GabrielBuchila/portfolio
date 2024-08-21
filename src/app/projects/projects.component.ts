55ed58f7c9dffd3189d1fdc76562c890abb89aca
Gabriel Buchila <gabrielbuchila@Gabriels-MacBook-Pro-2.local>
2024-08-21 17:08:55 +0300

    Implemented Shopping list project section

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  images: string[] = [
+    'assets/images/SList/1.png',
+    'assets/images/SList/2.png',
+    'assets/images/SList/3.png',
+    'assets/images/SList/4.png',
+    'assets/images/SList/5.png',
+    'assets/images/SList/6.png',
+    'assets/images/SList/7.png',
+    'assets/images/SList/8.png'
+  ];
+
+  currentIndex = 0;
+
+  constructor() {
+    setInterval(() => this.nextSlide(), 3000); // Change slide every 3 seconds
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevSlide() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
 
 }
