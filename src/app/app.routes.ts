0f07a30140a7ab6f913bea05d9492519c441d600
Gabriel Buchila <gabrielbuchila@Gabriels-MacBook-Pro-2.local>
2024-09-01 10:36:17 +0300

    Update implementation after solve some minor issues

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,35 +1,26 @@
-<<<<<<< Updated upstream
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-=======
-import { provideRouter, Routes, withHashLocation } from '@angular/router';
->>>>>>> Stashed changes
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { NgModule } from '@angular/core';
-<<<<<<< Updated upstream
-import { provideHttpClient } from '@angular/common/http';
-=======
-import { RouterModule } from '@angular/router';
->>>>>>> Stashed changes
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 export const routes: Routes = [
-    {path:'home', component:HomeComponent}, //default route
-    {path:'about', component:AboutComponent},
-    {path:'contact', component:ContactComponent},
-    {path:'projects', component:ProjectsComponent},
-    {path:'', redirectTo:'/home', pathMatch:'full'},
-    {path:'*', redirectTo:'/home'} //route to handle to 404
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', redirectTo: '/home' } // Handles unknown routes
 ];
 
-NgModule({
+@NgModule({
     imports: [RouterModule.forRoot(routes)],
-<<<<<<< Updated upstream
+
     exports: [RouterModule],
-    providers: [provideHttpClient()]
-=======
-    exports: [RouterModule]
->>>>>>> Stashed changes
-  })
-  export class AppRoutingModule { }
+    providers: [
+        { provide: LocationStrategy, useClass: PathLocationStrategy }
+    ]
+})
+export class AppRoutingModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule], // Add RouterModule to imports
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
